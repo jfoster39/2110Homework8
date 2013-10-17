fe021c8cfc07515876b9458651cc792713b3723b
jfoster39 <jfoster39@gatech.edu>
2013-10-17 18:57:52 -0400

    branch test

---
+++ mylib.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// Here is a comment
+
+typedef unsigned short u16;
+
+u16* videoBuffer;
+
+void setPixel(int r, int c, u16 color) {};
+
+void drawRect(int r, int c, int width, int height, u16 color) {};
+
+void drawHollowRect(int r, int c, int width, int height, u16 color) {};
+
