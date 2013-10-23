c211d304462e55847d6d4e597837424cdaedbd04
jfoster39 <jfoster39@gatech.edu>
2013-10-23 00:52:33 -0400

    progress

--- main.c
+++ main.c
@@ -2,15 +2,26 @@
 #define RGB(r, g, b) ((r) | ((g)<<5) | ((b)<<10))
 #define MODE3 3
 #define BG2_ENABLE (1<<10)
+#define RED RGB(31, 0, 0)
 typedef unsigned short u16;
-// need extern global variable
-u16 *pixelAddress = (u16 *)0x6000000;
+extern u16 *videoBuffer;
+
+void setPixel(int r, int c, u16 color);
+void drawRect(int r, int c, int width, int height, u16 color);
+void drawHollowRect(int r, int c, int width, int height, u16 color);
 
 int main(void) {
+	// Set up the Display control register to mode3, and BG2 enabled
     REG_DISPCTL = MODE3 | BG2_ENABLE;
-    *pixelAddress = RGB(31, 31, 31);
-    pixelAddress += 239;
-    *pixelAddress = RGB(31, 0, 0);
+
+    int i;
+    for (i = 0; i<5; i++) {
+    	setPixel(i, 0, RED);
+    }
+
+    drawRect(100, 100, 5, 3, RED);
+    drawHollowRect(20, 7,6, 3, RED);
+
     while(1) {}
     return 0;
 }
--- mylib.c
+++ mylib.c
@@ -5,14 +5,35 @@ void setPixel(int r, int c, u16 color);
 void drawRect(int r, int c, int width, int height, u16 color);
 void drawHollowRect(int r, int c, int width, int height, u16 color);
 
+/**
+Follows formula for calculating where to set the pixel. 
+**/
 void setPixel(int r, int c, u16 color) {
-    videoBuffer[r*240 + c] = color;
+    videoBuffer[(r*240 + c)] = color;
 }
 
 void drawRect(int r, int c, int width, int height, u16 color) {
-
+	int i;
+	int j;
+	int cReset = c;
+	for (i = 0; i < width; i++) {
+		for (j = 0; j < height; j++) {
+			setPixel(r, c ,color);
+			c++;
+		}
+	c = cReset;
+	r++;
+	}
 }
 
+/**
+Here we draw 4 seperate rectangles for the outline of a hollow rectangle.
+We first draw the top row, then the leftmost coloumn. We then draw the 
+bottom rectangle, and then the rightmost. 
+**/
 void drawHollowRect(int r, int c, int width, int height, u16 color) {
-
+    drawRect(r, r, width, 1, color);
+	drawRect(r, r, 1, height, color);
+    drawRect(r+height, r, 1, height, color);
+    drawRect(r, c+width, width, 1, color);
 }
